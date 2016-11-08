81ab7fe07f52c26dc95e83116789cb2bbeaf5f0c
Jessica Robbins <Kabukihousewife@gmail.com>
2016-11-08 15:29:16 -0700

    recent scripts

---
+++ New Unity Project 3/Assets/scripts/Score.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		StaticVars.score = PlayerPrefs.GetInt ("score");
+
+
+
+
+		StartCoroutine (AddToScore ());
+	
+	}
+	IEnumerator AddToScore ()
+
+	{
+		StaticVars.score++;
+		yield return new WaitForSeconds(1);
+		print (StaticVars.score);
+		StartCoroutine (AddToScore ());
+	}
+	void OnDisable()
+	{
+
+		PlayerPrefs.SetInt("Score", StaticVars.score);
+		print ("Final Score: " + PlayerPrefs.GetInt("Score"));
+		print ("game over");
+	}
+
+	}
