fc86354bb9285c7e441d81af6355484d129198d2
Jessica Robbins <Kabukihousewife@gmail.com>
2016-11-08 16:26:28 -0700

    variables

--- pluralsightlearning/Assets/variables.cs
+++ pluralsightlearning/Assets/variables.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class variables : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	// variables are just containers for information.  
+	//This could be numbers to give it a value, a string that combine actions and bools which turns actions on and off. 
+	//They need a name so you can reference it later. 
+	{
+		int ThisIsAVariable;
+		//here we can add a value
+		int MyVariable = 17;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
 }
