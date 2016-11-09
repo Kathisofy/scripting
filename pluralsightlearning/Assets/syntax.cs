c3d1627193826771e4be6e6df0c8e7b3b182e1d4
Jessica Robbins <Kabukihousewife@gmail.com>
2016-11-08 17:23:40 -0700

    syntax

---
+++ pluralsightlearning/Assets/syntax.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+using System.Collections;
+
+public class variables : MonoBehaviour {
+
+	// syntax is how you write code.  it's like a language.  the dot . is like going deeper into file folders on a computer
+	// indenting your code is similar, showing what things are linked togeather
+	// the semicolon is used like a period in a sentence. It ends the idea
+	/* oh hey, we're using syntax right here!
+	 * I could have been doing this all along rather than doing multipule lines of forward slash
+	 * this can also be used to disable code and put back in without having to rewrite it.
+	 */
+
