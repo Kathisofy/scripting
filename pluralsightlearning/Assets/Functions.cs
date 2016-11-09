d668ce0cf3aa2ab6b38bc860cddd01df154fb39a
Jessica Robbins <Kabukihousewife@gmail.com>
2016-11-08 17:09:51 -0700

    functions

---
+++ pluralsightlearning/Assets/Functions.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class variables : MonoBehaviour {
+	
+	// Functions or "methods" are used along side variables.  It takes the information and gives us something in return
+	//
+{
+		// Here's an example of a function that doesnt return
+		void start()
+	{
+
+	}
+		// we use this one all the time in class to test if something has worked in our code.
+		// for example
+		public void example()
+		{
+			print ("It's working");
+	}
+	// you have to write public for it to show up, otherwise it will be private.
+	
+}
+
