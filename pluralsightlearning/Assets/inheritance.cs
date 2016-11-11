9efa44288cceae636e6690180046aa3612579a59
Jessica Robbins <Kabukihousewife@gmail.com>
2016-11-10 22:45:55 -0700

    inheritance

---
+++ pluralsightlearning/Assets/inheritance.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class : MonoBehaviour {
+	{
+		
+		/* We've been using inheritence the whole time.  inheritence is just what it sounds like.
+		 * classes passing down qualities without having to type the code again.
+		 * for example here we're using "public class : monobehaviour"
+		 * we're using this because it automatically has traits that we want in unity
+		 * this also works with classes that you use elsewhere in the code
+	 */
+
+		
+		
+	}
