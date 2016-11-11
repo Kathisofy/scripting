7e8851bcdddeaad21954a52ec0bea057c5c116f1
Jessica Robbins <Kabukihousewife@gmail.com>
2016-11-10 21:58:11 -0700

    scope

---
+++ pluralsightlearning/Assets/scope.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class : MonoBehaviour {
+{
+	
+	/* Scope is where you can use variables.  usually it's the braces that you start and end an idea with.
+	 * it seperates ideas.  you can access variables outside of the areas by making variables public.
+	 * it also allows you to change things on the variable without having to mess with the code.
+	 * you can also access them from other scripts so you dont have to write it over and over again.
+	 * we used this in the array lesson.
+	 * classes without specifically being called public will automatically be private.
+	 */
+
+
+	public Flowers(string newName, int newColors)
+	{
+		name = newName;
+		colors = newColors;
+	}
+	
+	
+	
+	
+
+	
+}
