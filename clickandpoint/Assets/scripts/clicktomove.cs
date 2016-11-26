5a9802071f035f6a1780598a2d8ff7e7a8a7d46c
Jessica Robbins <Kabukihousewife@gmail.com>
2016-11-25 19:21:06 -0700

    trying to figure out my game

---
+++ clickandpoint/Assets/scripts/clicktomove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clicktomove : MonoBehaviour {
+
+	public float walkDistance = 10f;
+	private NavMeshAgent navAgent;
+	private Transform targetItem;
+	private Ray clickRay;
+	private RaycastHit clickHit;
+	private bool walking;
+	private bool itemClicked;
+
+	// Use this for initialization
+	void Awake() 
+	{
+		navAgent = GetComponent<NavMeshAgent> ();
+	}
+
+
+	void Update()
+		{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetButtonDown ("Fire1")){
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				if (hit.collider.CompareTag ("Item")) 
+					{
+					targetItem = hit.transform;
+					itemClicked = true;
+					}	 
+
+				else 
+					{ 
+					walking = true;
+					itemClicked = false;
+					navAgent.destination = hit.point;
+					navAgent.Resume();
+					}
+			}
+		}
+	}
+}
+
---
+++ final_game/Assets/script/IAddPoints.cs
@@ -0,0 +1,6 @@
+﻿public interface IAddPoints {
+
+	int points { get; set;}
+	void OnTriggerEnter()
+
+}
