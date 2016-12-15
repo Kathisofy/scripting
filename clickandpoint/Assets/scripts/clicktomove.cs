4157ed837370c76b4aa304853bc6bf632033d149
Jessica Robbins <Kabukihousewife@gmail.com>
2016-12-14 21:05:30 -0700

    final

--- clickandpoint/Assets/scripts/clicktomove.cs
+++ clickandpoint/Assets/scripts/clicktomove.cs
@@ -2,44 +2,19 @@
 using System.Collections;
 
 public class clicktomove : MonoBehaviour {
-
-	public float walkDistance = 10f;
-	private NavMeshAgent navAgent;
-	private Transform targetItem;
-	private Ray clickRay;
-	private RaycastHit clickHit;
-	private bool walking;
-	private bool itemClicked;
-
-	// Use this for initialization
-	void Awake() 
-	{
-		navAgent = GetComponent<NavMeshAgent> ();
+	NavMeshAgent agent;
+	
+	void Start() {
+		agent = GetComponent<NavMeshAgent>();
 	}
-
-
-	void Update()
-		{
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		RaycastHit hit;
-		if (Input.GetButtonDown ("Fire1")){
-			if (Physics.Raycast(ray, out hit, 100))
-			{
-				if (hit.collider.CompareTag ("Item")) 
-					{
-					targetItem = hit.transform;
-					itemClicked = true;
-					}	 
-
-				else 
-					{ 
-					walking = true;
-					itemClicked = false;
-					navAgent.destination = hit.point;
-					navAgent.Resume();
-					}
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				agent.destination = hit.point;
 			}
 		}
 	}
 }
-
